fa72e8fe8ac402d28b74a86105db8c59a2ee9fb9
Artem <Joker134127@pm.me>
2025-12-26 18:24:44 +0200

    Add React frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Booking Oselya</h1>
+        <p>Hotel Booking System - Coming Soon!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
